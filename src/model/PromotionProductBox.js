2c80444452769d09341b77ce0191ff06f26bdd07
moonseonghui <msh9980@naver.com>
2024-11-08 05:34:08 +0900

    feat: 프로모션이 있는 상품의 수량을 관리하는 PromotionProductBox 클래스 구현
    - ProductBox와 Promotion을 조합하여 관리하는 PromotionProductBox 클래스 구현

---
+++ src/model/PromotionProductBox.js
@@ -0,0 +1,12 @@
+class PromotionProductBox {
+  #productBox;
+
+  #promotion;
+
+  constructor(productBox, promotion) {
+    this.#productBox = productBox;
+    this.#promotion = promotion;
+  }
+}
+
+export default PromotionProductBox;
