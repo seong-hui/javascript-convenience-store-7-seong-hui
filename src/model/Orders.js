9615cb9b1605241fc3ae5d69dc5264a5b1f288e5
moonseonghui <msh9980@naver.com>
2024-11-10 06:54:52 +0900

    feat(Orders): 영수증 출력을 위한 금액 정보 계산 메서드 구현

--- src/model/Orders.js
+++ src/model/Orders.js
@@ -28,5 +28,28 @@ class Orders {
       return sum + orderItem.calculateDiscountPrice();
     }, 0);
   }
+
+  calculateTotalPrice() {
+    return this.#orderItems.reduce((sum, orderItem) => {
+      return sum + orderItem.calculateOrderPrice();
+    }, 0);
+  }
+
+  calculateMembershipDiscountPrice() {
+    const totalPrice = this.calculateTotalPrice();
+    const totalDiscountPrice = this.calculateTotalDiscountPrice();
+    const discountedAmount = totalPrice - totalDiscountPrice;
+    const membershipDiscount = Math.round(discountedAmount * 0.3);
+    if (membershipDiscount >= 3000) return 3000;
+    return membershipDiscount;
+  }
+
+  calculateTotalDue() {
+    const totalPrice = this.calculateTotalPrice();
+    const totalDiscountPrice = this.calculateTotalDiscountPrice();
+    const membershipDiscountPrice = this.calculateMembershipDiscountPrice();
+
+    return totalPrice - totalDiscountPrice - membershipDiscountPrice;
+  }
 }
 export default Orders;
