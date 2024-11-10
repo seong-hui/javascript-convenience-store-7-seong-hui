bceb0fdc2958927705d5fc34d0632b47bb3fa03d
moonseonghui <msh9980@naver.com>
2024-11-10 20:13:07 +0900

    refactor: 현재 날짜와 시간을 받아오는 기능 util함수로 분리

---
+++ src/utils/generateNowDateTime.js
@@ -0,0 +1,6 @@
+import { DateTimes } from '@woowacourse/mission-utils';
+
+const generateNowDateTime = function generateNowDateTime() {
+  return DateTimes.now();
+};
+export default generateNowDateTime;
