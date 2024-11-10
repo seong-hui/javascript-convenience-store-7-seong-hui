3dea64eca6b5e49c51cc4dcecb1bec5276d3e4d6
moonseonghui <msh9980@naver.com>
2024-11-10 19:05:53 +0900

    fix: StockManager클레스에서 생성자 없이 boxesInventory를 받아서 재고 확인을 하는 로직으로 수정

--- src/model/StockManager.js
+++ src/model/StockManager.js
@@ -1,27 +1,15 @@
 import Validator from '../validator/Validator.js';
-import ProductBox from './ProductBox.js';
-import PromotionProductBox from './PromotionProductBox.js';
 
 class StockManager {
-  #allProductBoxes;
-
-  constructor(allProductBoxes) {
-    this.#allProductBoxes = allProductBoxes;
-  }
-
-  #findProductBoxbyName(productName) {
-    const matchingBoxes = this.#allProductBoxes.filter((box) => {
-      return box.getProductName() === productName;
-    });
-
-    const productBox = matchingBoxes.find((box) => box instanceof ProductBox);
-    const promotionProductBox = matchingBoxes.find((box) => box instanceof PromotionProductBox);
+  static #findProductBoxbyName(productName, boxesInventory) {
+    const productBox = boxesInventory.findProductBoxByName(productName);
+    const promotionProductBox = boxesInventory.findPromotionBoxByName(productName);
     return { productBox, promotionProductBox };
   }
 
-  findValidBoxesForCartItems(shoppingCart) {
+  static findValidBoxesForCartItems(shoppingCart, boxesInventory) {
     shoppingCart.getItems().forEach(({ product, quantity }) => {
-      const { productBox, promotionProductBox } = this.#findProductBoxbyName(product.getName());
+      const { productBox, promotionProductBox } = StockManager.#findProductBoxbyName(product.getName(), boxesInventory);
       const totalStock = StockManager.#calculateTotalStock(productBox, promotionProductBox);
       Validator.checkStockAvailable(totalStock, quantity);
     });
